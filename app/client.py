021d98d54f172ebade3fa90dbbeba360eea836ed
david <davidlima@ufba.br>
2025-06-25 15:54:42 -0300

    feat: Reliable multicast method

--- app/client.py
+++ app/client.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
+import json
 import socket
 import logging
 import threading
 from time import sleep
+from typing import *
 
 from utils import constants
 
@@ -24,7 +26,8 @@ class Client:
     def _start_receive_loop(self):
         def receive_loop():
             while self.is_connected():
-                self.receive()
+                m = self._sock_receive()
+                self.r_deliver(m)
 
         thread = threading.Thread(target=receive_loop, daemon=True)
         thread.start()
@@ -57,25 +60,22 @@ class Client:
         id = str(self)
         self.disconnect()
         print(f"{id} crashed!")
-
     
-    def _construct_message(self, dest_addr: str, m: str) -> bytes:
-
+    def _construct_message(self, dest_addr: str, m: str, **kargs) -> bytes:
         msg = str({
             'sender': self.get_addr(),
             'dest': dest_addr,
-            'content': m
+            'content': m,
+            **kargs
         }).encode(constants.MSG_ENCODING)
 
         msg = b'<bof>' + msg + b'<eof>'
-        print(msg)
-
         return msg
 
-    def receive(self):
+    def _sock_receive(self) -> str:
         if not self.is_connected():
             logger.info(f"[{self}] Could not start receiving messages in socket. Socket does not exist!")
-            return
+            return ''
 
         assert self._socket is not None # Making Pyright shut up
 
@@ -90,20 +90,53 @@ class Client:
                 data += block
                 if b'<eof>' in block:
                     break
-            print(f"[{self}] Received message:", data.decode(constants.MSG_ENCODING))
+
             conn.close()
+
+            return data.decode(constants.MSG_ENCODING)
 
         except Exception as e:
             logger.error(f"{self} Error receiving message: {e}")
 
+        return ''
+
     def send(self, dest_addr: str, message: str):
+        pkg = self._construct_message(dest_addr, message)
+        self.send_raw(dest_addr, pkg)
+
+    def send_raw(self, dest_addr: str, pkg: bytes):
+        """
+        Para ser usado para o envio de dados brutos, em bytes.
+        Para o nosso propósito, a não ser que saiba o que está fazendo,
+        Utilize após utilização do método _construct_message()
+        """
         try:
             send_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             send_sock.connect(dest_addr)
 
-            encoded_msg = self._construct_message(dest_addr, message)
-            print(f"[{self}] Sending message to {dest_addr}.")
-            send_sock.send(encoded_msg)
+            print(f"{self} Sending message to {dest_addr}.")
+            send_sock.send(pkg)
 
         except Exception as e:
             logger.error(f"{self} Error sending message: {e}")
+
+
+    def r_multicast(self, message: str, group: list[str], crash_after: Optional[int] = None):
+        group.remove(self.get_addr()) # Remove si mesmo do grupo dos processos que devem receber a mensagem
+        for i, addr in enumerate(group):
+            if (crash_after is not None and i+1 == crash_after):
+                self.crash()
+                return
+
+            pkg = self._construct_message(m=message, dest_addr=addr, group=group)
+            self.send_raw(addr, pkg)
+
+        self.r_deliver(message)
+
+    def r_deliver(self, m: str):
+        try:
+            m = m[m.index('>')+1:m.rindex('<')]
+        except ValueError:
+            logger.fatal(f"Message received, but in invalid format: {m}")
+
+        logger.info(f"{self} Received message: {m}")
--- main.py
+++ main.py
@@ -3,14 +3,16 @@
 import logging
 logging.basicConfig(level=logging.INFO, handlers=[logging.StreamHandler()])
 
+from app import client
 from app.client import Client
 
 if __name__ == '__main__':
-    group = [Client(), Client()]
+    client_group = [Client(), Client(), Client()]
+    addrs = [c.get_addr() for c in client_group]
 
-    for p in group:
+    for p in client_group:
         print(p)
 
     m = 'Hello world!'
-    group[0].send(group[1].get_addr(), m)
+    client_group[0].r_multicast(m, addrs,  crash_after=2)
     #P[0].multicast(P[1:], m)
